1bd6dee3cc3db6bdf9fafc9379553004fd587800
Laura Urb <lauraiurb@gmail.com>
2021-06-07 10:02:30 -0300

    docs agregado

---
+++ docs/precache-manifest.5af24ea55bc6f796e355080722763807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f5bb56f4a5ea90efe9a5a7b48ccd01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e2d73f3e2ae758ae13a",
+    "url": "/static/css/main.6161d4fc.chunk.css"
+  },
+  {
+    "revision": "7a2205386b2d7bce2c76",
+    "url": "/static/js/2.e8326f78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8326f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2d73f3e2ae758ae13a",
+    "url": "/static/js/main.eaf46666.chunk.js"
+  },
+  {
+    "revision": "a28c9cbebdabf410c0e7",
+    "url": "/static/js/runtime-main.de916d74.js"
+  }
+]);
